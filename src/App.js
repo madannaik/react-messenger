488d664e7031c5337b18d0334a9e12de68131188
Madan Naik <48375643+madannaik@users.noreply.github.com>
2021-05-07 11:21:54 +0530

    Revert "ui provider - chakra UI"
    This reverts commit 178f65dabb7488c9e5dae241d7e3ddab9c7d7add.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import {SearchOutlined } from "@material-ui/icons";
-import { IconButton } from "@chakra-ui/react";
-import { DrawerExample } from './components/drawer';
-const App = () =>{
-  return(
-      <div>
-        <DrawerExample/>
-      </div>
+import logo from './logo.svg';
+import './App.css';
 
-  )
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
 export default App;
