bdb28e3a10f6896da111edf8ee3a4276e65974e6
Madan Naik <48375643+madannaik@users.noreply.github.com>
2021-05-09 09:32:52 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import { Login } from './components/SignIn&login/login';
 import { SingUp } from './components/SignIn&login/signup';
-
+import  {Route, HashRouter as Router,Switch} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <SingUp/>
+      <Router>
+        <Switch>
+          <Route path="/" component={SingUp}  /> 
+          {/* <Route path="/signup" component={SingUp} /> */}
+        </Switch>
+        
+      </Router>
+
     </div>
   );
 }
